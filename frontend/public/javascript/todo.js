b979cbb5b4df20f93368ec5f58cf49a3e492c1a1
sanghyeonkim0502 <sanghyeonkim0502@gmail.com>
2023-10-24 17:00:22 +0900

    inputStyle 해야함

--- frontend/public/javascript/todo.js
+++ frontend/public/javascript/todo.js
@@ -8,13 +8,14 @@ export default {
             emptyPage: true,
             inputValue: ref(''),
             arr: ref([]),
+            inputStyle: ref([]),
             checked: false
         }
     },
 
     methods: {
         reload() {
-            console.log('reload')
+            inputStyle = ref([])
             fetch('http://localhost:3000/api/list', {
                 method: "GET"
             }).then((data) => data.json()).then((res) => {
@@ -32,12 +33,16 @@ export default {
                     this.listPage = true
 
                     result.map((data) => {
-                        if(data.state == 1){
+                        if (data.state == 1) {
                             data.state = true
-                        }else{
+                        } else {
                             data.state = false
                         }
-                    })
+                    }).forEach(element => {
+                          
+                    });
+                    
+
 
                     // 스위치 on상태 일때 판단
                     if (localStorage.getItem('onoff') == 'true') {
@@ -82,13 +87,34 @@ export default {
                 console.error(`err : ${err}`)
             })
         },
-        stateEdit(id, state){
-            console.log(id, state)
+        stateEdit(id, state) {
+            stateMap = state == true ? 1 : 0
+
+            if (state == true) {
+
+            } else {
+
+            }
+            fetch('http://localhost:3000/api/list/state/edit', {
+                method: "PATCH",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: JSON.stringify({
+                    "id": id,
+                    "state": stateMap
+                })
+            }).then(() => {
+                //스위치 온 오프 상태일때 체크박스 체크를 하면 바로 화면에 반영하기 위함
+                this.reload()
+            }).catch(err => {
+                console.error(`err : ${err}`)
+            })
         },
-        contentEdit(){
+        contentEdit() {
 
         },
-        remove(){
+        remove() {
 
         }
     },
